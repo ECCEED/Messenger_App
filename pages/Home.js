23be64aa96ed098428fd5cf02a13ea4e6c44b53c
Keskiidou <128159955+Keskiidou@users.noreply.github.com>
2024-08-28 16:02:29 +0100

    error

---
+++ pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+import Footer from '../component/Footer';
+
+function Home() {
+
+  return (
+
+    <SafeAreaProvider>
+      <NativeBaseProvider>
+        <Text>Hi home screen  page  niger</Text>
+        <Footer />
+      </NativeBaseProvider>
+    </SafeAreaProvider>
+
+  );
+}
+export default Home;
