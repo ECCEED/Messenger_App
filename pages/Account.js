a3c18f9d4409c0b0dcb5ed71cbfc7a87c740347e
Keskiidou <128159955+Keskiidou@users.noreply.github.com>
2024-08-30 18:53:02 +0100

    slight chnages cdt

--- pages/Account.js
+++ pages/Account.js
@@ -10,7 +10,7 @@ function Account() {
 
     <SafeAreaProvider>
       <NativeBaseProvider>
-        <Text>Hi HOME</Text>
+        <Text>Hi ACCOUNT</Text>
         <Footer />
       </NativeBaseProvider>
     </SafeAreaProvider>
--- pages/Home.js
+++ pages/Home.js
@@ -18,21 +18,4 @@ function Home() {
   );
 }
 export default Home;
-// import React from 'react';
-// import { Text, View } from 'react-native';
-// import { SafeAreaProvider } from 'react-native-safe-area-context';
-// import { NativeBaseProvider } from 'native-base';
-
-// function Home() {
-//   return (
-//     <SafeAreaProvider>
-//       <NativeBaseProvider>
-//         <View>
-//           <Text>Hi home screen</Text>
-//         </View>
-//       </NativeBaseProvider>
-//     </SafeAreaProvider>
-//   );
-// }
-
-// export default Home;
+ 
